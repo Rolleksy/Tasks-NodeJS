572c2b1176746b2fe45377bb811e5968cc729a98
Rolleksy <aleksanderpurcha@gmail.com>
2024-04-22 19:23:22 +0200

    added compose fn

--- Task2.js
+++ Task2.js
@@ -2,19 +2,24 @@ const getFullName = person => {
   return `${person.firstName} ${person.lastName}`;
 };
 
-const filterUniqueWords = text =>
-    text
-        .split(" ")
-        .filter((word, index, arr) => arr.indexOf(word) === index)
-        .sort((a,b) => a.localeCompare(b));
+const compose = (...fns) => x => fns.reduceRight((v, f) => f(v), x);
 
-const getAverageGrade = students =>
-    students
-        .map(student => student.grades)
-        .flat()
-        .reduce((total, grade) => total + grade, 0) / students
-        .map(student => student.grades.length)
-        .reduce((total, count) => total + count, 0);
+// filter unique words
+const sortWords = words => words.sort((a, b) => a.localeCompare(b));
+const toLowerAndSplit = text => text.toLowerCase().split(" ");
+const filterUnique = words => words.filter((word, index, arr) => arr.indexOf(word) === index);
+
+
+const filterUniqueWords = compose(sortWords, filterUnique, toLowerAndSplit);
+
+// get average grade
+const getAverage = grades => 
+  grades.reduce((total, grade) => total + grade, 0) / grades.length;
+
+const flatten = arr => arr.reduce((acc, val) => acc.concat(val), []);
+
+const getAverageGrade = compose(
+  getAverage, flatten, students => students.map(student => student.grades));
 
 // Test - Task 2
 
