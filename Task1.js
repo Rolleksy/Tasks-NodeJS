a5bab1ea1a84b826a8e216f823a4acca3211b4f5
Rolleksy <aleksanderpurcha@gmail.com>
2024-04-26 15:26:05 +0200

    added "address" to updateInfo

--- Task1.js
+++ Task1.js
@@ -16,7 +16,7 @@ const person = {
     value: function(newInfo) {
       Object.keys(newInfo).forEach(prop => {
         if (this.hasOwnProperty(prop)) {
-          if (Object.getOwnPropertyDescriptor(this, prop).writable === false) {
+          if (Object.getOwnPropertyDescriptor(this, prop).writable === false || prop === "address") {
             this[prop] = newInfo[prop];
           }
         }
