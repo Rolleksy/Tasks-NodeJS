09e967160421dd6de37634b44374458dad4c1ed0
Rolleksy <aleksanderpurcha@gmail.com>
2024-09-02 17:05:38 +0200

    trying to make test work

--- Backend/jest.setup.js
+++ Backend/jest.setup.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: ".env" });
+require('dotenv').config({ path: "./.env" });
 // turns off console.error
 const originalConsoleError = console.error;
 console.error = jest.fn();
