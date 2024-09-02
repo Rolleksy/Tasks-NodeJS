b5700b6adbcc331021030ceba36950ffa55ea866
Rolleksy <aleksanderpurcha@gmail.com>
2024-09-02 16:30:17 +0200

    trying different pathing jest.setup

--- Backend/jest.setup.js
+++ Backend/jest.setup.js
@@ -1,4 +1,5 @@
-require('dotenv').config({ path: '.env' });
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 // turns off console.error
 const originalConsoleError = console.error;
 console.error = jest.fn();
